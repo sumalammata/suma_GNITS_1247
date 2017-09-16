7bb1bef687ab06bf78a85546fcde98b77d3696f0
sumalammata <tejaswarilammata@gmail.com>
2017-09-16 22:02:51 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		int yyyy,mm,dd;
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Scanner sc= new Scanner(System.in);
+		String name=sc.next();
+		int id=sc.nextInt();
+		Date birthDate=SimpleDateFormat(yyyy-mm-dd);
+		double avgmark=sc.nextDouble();
+		
+		
+	}
+
+}
